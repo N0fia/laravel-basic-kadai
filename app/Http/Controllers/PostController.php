6c0a2f3ec27efa36463549ab248437e8fded97f8
Nofia <bestlife.hamaguchi@gmail.com>
2025-05-29 14:55:46 +0900

    Add files via upload

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
     public function index() {
-        $name = '侍 太郎';
-        $languages = ['HTML', 'CSS', 'JavaScript', 'PHP'];
+        // postsテーブルからすべてのデータを取得して変数$postsへ代入
+        $posts = DB::table('posts')->get();
 
-        // 変数$name、$languagesをindex.blade.phpファイルに渡す
-        return view('index', compact('name', 'languages'));
-    }
+        // 変数$postsをposts/index.blade.phpファイルに渡す
+        return view('posts.index', compact('posts'));
+    }      
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,26 +1,31 @@
 <!DOCTYPE html>
- <html lang="ja">
- 
- <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Laravel基礎</title>
- </head>
- 
- <body>
-     <table>
-         <tr>
-             <th>タイトル</th>
-             <th>本文</th>
-             
-         </tr>
-         @foreach($posts as $post)
-             <tr>
-                 <td>{{ $post->title }}</td>
-                 <td>{{ $post->content }}</td>
-             </tr>            
-         @endforeach       
-     </table>
- </body>
- 
- </html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Laravel基礎</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>本文</th>
+            <th>作成日時</th>
+            <th>更新日時</th>
+        </tr>
+        @foreach($posts as $posts)
+        <tr>
+            <td>{{ $posts->id }}</td>
+            <td>{{ $posts->title }}</td>
+            <td>{{ $posts->content }}</td>
+            <td>{{ $posts->created_at }}</td>
+            <td>{{ $posts->updated_at }}</td>
+        </tr>
+         @endforeach  
+    </table>
+</body>
+
+</html>
