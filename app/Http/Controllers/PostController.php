bd83aa548e3c982dffe27b642c9b5926b7d791aa
Nofia <bestlife.hamaguchi@gmail.com>
2025-05-31 13:44:02 +0900

    Add files via upload

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,12 +9,20 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index($id) {
+
+        public function index() {
+        // productsテーブルからすべてのデータを取得し、変数$productsに代入する
+        $posts  = DB::table('posts')->get();
+
+        // 変数$productsをproducts/index.blade.phpファイルに渡す
+        return view('posts.index',compact('posts'));
+    }
+        public function show($id) {
         // URL'/products/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをproductsテーブルから取得し、変数$productに代入する
-        $posts = Post::find($id);
+        $posts  = Post::find($id);
 
         // 変数$productをproducts/show.blade.phpファイルに渡す
         //return view('products.show', compact('product'));
-        return view('Posts.show', compact('posts'));
+        return view('posts.show', compact('posts'));
     }
 }
